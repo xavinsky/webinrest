45100ef4dbe8585fc864b2b9928218644cca3897
xaviermanach <xav@tekio.org>
2012-12-04 12:56:23 +0100

    update code for python2.6 compatibility (DOTALL not exist in 2.6 in re module)
    Contact : <xav@tekio.org>

--- install/install_debian.sh
+++ install/install_debian.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo aptitude install python-docutils python-magic
+sudo aptitude install python-docutils python-magic python-argparse git
 cd /usr/local/lib
 git clone https://github.com/xaviermanach/rest2web.git
 ln -s /usr/local/lib/rest2web/rest2web /usr/local/bin
