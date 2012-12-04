32a47e89c6a276d999675f4092ef1e1cc9b9248a
xaviermanach <xav@tekio.org>
2012-12-04 22:52:14 +0100

    supervisor configuration and update init.d script. BugFix For Config : encoding utf-8 forced (because init.d script and supervisor don't use env utf-8 by default)
    Contact : <xav@tekio.org>

--- install/install_debian.sh
+++ install/install_debian.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-sudo aptitude install python-docutils python-magic python-argparse git
+sudo aptitude install python-docutils python-magic python-argparse git supervisor
 cd /usr/local/lib
 git clone https://github.com/xaviermanach/rest2web.git
 ln -s /usr/local/lib/rest2web/rest2web /usr/local/bin
 ln -s /usr/local/lib/rest2web/searchinfiles.py /usr/local/lib/python2.`python -V 2>&1 | awk -F '.' '{print $2}'`/dist-packages/
 mkdir /etc/rest2web
 cp /usr/local/lib/rest2web/config/rest2web.conf /etc/rest2web
-cp /usr/local/lib/rest2web/config/init.d/rest2web /etc/init.d
 cp -r /usr/local/lib/rest2web/config/includes /etc/rest2web
 mkdir -p /var/www/rest2web
-update-rc.d rest2web defaults
+cp /usr/local/lib/rest2web/config/supervisord/rest2web.conf /etc/supervisor/conf.d/
+supervisorctl reread
