1e2b90da325310c6370a03d0dc8e817dc2b3d0d9
xaviermanach <xav@tekio.org>
2012-12-04 12:14:23 +0100

    Add script for debian install.
    Contact : <xav@tekio.org>

---
+++ install/install_debian.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo aptitude install python-docutils python-magic
+cd /usr/local/lib
+git clone https://github.com/xaviermanach/rest2web.git
+ln -s /usr/local/lib/rest2web/rest2web /usr/local/bin
+ln -s /usr/local/lib/rest2web/searchinfiles.py /usr/local/lib/python2.`python -V 2>&1 | awk -F '.' '{print $2}'`/dist-packages/
+mkdir /etc/rest2web
+cp -r /usr/local/lib/rest2web/config/* /etc/rest2web
+mkdir -p /var/www/rest2web
