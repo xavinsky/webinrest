d2f2fbb42455c5f5ab9f40a29ac3ddbe5ac8fa2a
xaviermanach <xav@tekio.org>
2012-12-11 15:21:39 +0100

    Fix name lib search in doc.
    Contact : <xav@tekio.org>

--- install/install_debian.sh
+++ install/install_debian.sh
@@ -3,7 +3,7 @@ sudo aptitude install python-docutils python-magic python-argparse git superviso
 cd /usr/local/lib
 git clone https://github.com/xaviermanach/webinrest.git
 ln -s /usr/local/lib/webinrest/webinrest /usr/local/bin
-ln -s /usr/local/lib/webinrest/searchinfiles.py /usr/local/lib/python2.`python -V 2>&1 | awk -F '.' '{print $2}'`/dist-packages/
+ln -s /usr/local/lib/webinrest/search_in_files.py /usr/local/lib/python2.`python -V 2>&1 | awk -F '.' '{print $2}'`/dist-packages/
 mkdir /etc/webinrest
 cp /usr/local/lib/webinrest/config/webinrest.conf /etc/webinrest
 cp -r /usr/local/lib/webinrest/config/includes /etc/webinrest
